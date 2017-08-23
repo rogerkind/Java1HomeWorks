b68b590874e9dc3a1aaecf1101ef07a65a968f20
gered <gered@mail.ru>
2017-08-23 10:41:38 +0600

    Class InputPanel prototype +method String getValue

--- HomeWork8Advanced/GuessWord/src/InputPanel.java
+++ HomeWork8Advanced/GuessWord/src/InputPanel.java
@@ -1,4 +1,22 @@
+import java.awt.Font;
+import java.awt.GridLayout;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
-public class InputPanel {
-
+@SuppressWarnings("serial")
+public class InputPanel extends JPanel {
+	private JTextField txtInField= new JTextField();
+		
+	InputPanel(Font font){
+		txtInField.setHorizontalAlignment(SwingConstants.LEADING);
+		txtInField.setFont(font);
+		txtInField.setColumns(1);
+		setLayout(new GridLayout(1, 0, 0, 0));
+		add(txtInField);
+	}
+	
+	public String getValue() {
+		return txtInField.getText();
+	}
 }
